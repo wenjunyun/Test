0a976955a9d791175af87ac7581a0245a4a9ba77
wenjunyun <wenjunyun1985@163.com>
2017-12-26 10:40:43 +0800

    你个sb

---
+++ src/com/cnct/wenjunyun/Mytest.java
@@ -0,0 +1,15 @@
+package com.cnct.wenjunyun;
+
+/**
+ * this is my test class
+ *
+ * @author WENJUNYUN
+ * @create 2017/12/26 10:35
+ **/
+public class Mytest {
+    public static  void main(String[] args){
+        System.out.println("....................");
+    }
+
+
+}
